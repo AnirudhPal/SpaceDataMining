2988a598b4d4859a0e4b66f75c7b448093ea54d7
AnirudhPal <pal5@purdue.edu>
2019-10-11 02:59:18 -0400

    Ola

--- AnirudhStuff/kmeans.py
+++ AnirudhStuff/kmeans.py
@@ -1,1 +1,83 @@
-Hello
+# Import Libs
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from sklearn.cluster import KMeans
+from sklearn import datasets
+import pandas as pd
+
+# My K-Means
+## Config Vars
+VERBOSE = True
+# Load Data Set
+def loadCSV(filename):
+    # Get Data Frame
+    data_frame = pd.read_csv(filename)
+
+    # Print Data Set
+    if VERBOSE:
+        print('loadCSV():')
+        print(data_frame.head())
+        print('...')
+        print(data_frame.tail())
+        print('')
+
+    # Return Data Frame
+    return data_frame
+
+np.random.seed(5)
+
+iris = datasets.load_iris()
+X = loadCSV(r'C:\Users\mailb\Documents\GitHub\SpaceDataMining\AnirudhStuff\TestData2.csv')#iris.data
+y = iris.target
+
+estimators = [('k_means_iris_8', KMeans(n_clusters=8)),
+              ('k_means_iris_3', KMeans(n_clusters=3))]
+
+fignum = 1
+titles = ['8 clusters', '3 clusters', '3 clusters, bad initialization']
+for name, est in estimators:
+    fig = plt.figure(fignum, figsize=(4, 3))
+    ax = Axes3D(fig, rect=[0, 0, .95, 1], elev=48, azim=134)
+    est.fit(X)
+    labels = est.labels_
+
+    ax.scatter(X, X, X, edgecolor='k')
+
+    ax.w_xaxis.set_ticklabels([])
+    ax.w_yaxis.set_ticklabels([])
+    ax.w_zaxis.set_ticklabels([])
+    ax.set_xlabel('Petal width')
+    ax.set_ylabel('Sepal length')
+    ax.set_zlabel('Petal length')
+    ax.set_title(titles[fignum - 1])
+    ax.dist = 12
+    fignum = fignum + 1
+    plt.show()
+
+# Plot the ground truth
+fig = plt.figure(fignum, figsize=(4, 3))
+ax = Axes3D(fig, rect=[0, 0, .95, 1], elev=48, azim=134)
+
+for name, label in [('Setosa', 0),
+                    ('Versicolour', 1),
+                    ('Virginica', 2)]:
+    ax.text3D(X[y == label, 3].mean(),
+              X[y == label, 0].mean(),
+              X[y == label, 2].mean() + 2, name,
+              horizontalalignment='center',
+              bbox=dict(alpha=.2, edgecolor='w', facecolor='w'))
+# Reorder the labels to have colors matching the cluster results
+y = np.choose(y, [1, 2, 0]).astype(np.float)
+ax.scatter(X[:, 3], X[:, 0], X[:, 2], c=y, edgecolor='k')
+
+ax.w_xaxis.set_ticklabels([])
+ax.w_yaxis.set_ticklabels([])
+ax.w_zaxis.set_ticklabels([])
+ax.set_xlabel('Petal width')
+ax.set_ylabel('Sepal length')
+ax.set_zlabel('Petal length')
+ax.set_title('Ground Truth')
+ax.dist = 12
+
+plt.show()
